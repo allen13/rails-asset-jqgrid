681165b1d548b57072d94206ef45b0c17a7b82ed
allen13 <kex.allen13@gmail.com>
2011-11-23 05:54:41 -0500

    Add rspec testing complete with a dummny app.

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '53537b0c8835cbdced1b459318c133f6a06acbf5ca7e37cafb890d5348f159aaa2491ecda675873cc16861c66e71181134af3ba30e976a3a859a83f2ccac8f64'
---
+++ spec/jqgrid_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Rails::Asset::Jqgrid do
+  it "should be valid" do
+    Rails::Asset::Jqgrid.should be_a(Module)
+  end
+end
